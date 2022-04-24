ac28757de16cefe36e4f02237ff389442bb6cff6
MaksimDvoretskii <mdmmdams@mail.ru>
2022-04-24 12:22:56 +0400

    commit 2

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,12 +1,12 @@
 package ru.netology.sqr;
 
 public class SQRService {
-    public int calculate(int underRange,int upperRange) {
-        int counter = 0; // считает количество раз когда i * i попадает в промежуток underRange, upperRange
-        for (int i = 10; i <= 99; i++) { // цикл начиная от i = 10 до i = 99 после каждой итерации прибавляет i + i
+    public int calculate(int underMeaning,int upperMeaning) {
+        int counter = 0;
+        for (int i = 10; i <= 99; i++) {
             int sqr = i * i;
-            if (underRange <= sqr && sqr <= upperRange) { // && - логическое и (TRUE, если оба условия верны)
-                counter++; // идентично counter = counter + 1
+            if (underMeaning <= sqr && sqr <= upperMeaning) {
+                counter++;
             }
         }
         return counter;
